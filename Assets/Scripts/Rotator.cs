e46b3414f0b38124c9fb6ddee23562456eb1cd91
cuong-petarain <163802020+cuong-petarain@users.noreply.github.com>
2025-08-18 16:00:33 +0700

    Branch one (#2)
    * feat: b1 one
    
    * fix: b1 two
    
    * feat: b1 three

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -17,5 +17,7 @@ public class Rotator : MonoBehaviour
     {
         // Rotate the object around the Y axis
         transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        // something 1
+        // something 2
     }
 }
